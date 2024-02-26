e92972f117949c171efa1f2cf78cf552e8c309e9
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-02-26 19:05:55 +0100

    [feat] access database

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,15 @@
 
 const express = require("express");
 const cors = require("cors");
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('db/db.sqlite', (err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to database');
+  }
+});
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +18,18 @@ const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+
+app.get('/api/topics', (req, res) => {
+  db.all('SELECT * FROM TOPICS', (err, rows) => {
+    if (err) {
+      console.error('Error getting topics:', err);
+      res.status(500).send('Error getting topics');
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 
@@ -21,6 +42,29 @@ app.get('/api/users', (req, res) => {
   console.log('api/users called!');
   res.json(users);
 });
+
+
+
+app.get('/api/printSchema', (req, res) => {
+  db.serialize(() => {
+    // Retrieve the schema using PRAGMA statement
+    db.all("PRAGMA table_info('TOPICS')", (err, rows) => {
+      if (err) {
+        console.error('Error printing schema:', err);
+        res.status(500).send('Error printing schema');
+      } else {
+        if (rows.length === 0) {
+          res.status(404).send('Table "TOPICS" does not exist or has no columns.');
+        } else {
+          // Extract column names from the rows
+          const schema = rows.map(row => row.name);
+          res.json(schema);
+        }
+      }
+    });
+  });
+});
+
   
 
 
