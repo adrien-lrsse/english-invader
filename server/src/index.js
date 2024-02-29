2e4af99e0aefc08301e79e93ff59b5a7fe5c9b66
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-02-29 17:47:35 +0100

    [feat] db tables update

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const sqlite3 = require('sqlite3').verbose();
 const topicRouter = require('./routes/topics.router');
 const runningRouter = require('./routes/running.router');
-const authRouter = require('./routes/auth.router');
+//const authRouter = require('./routes/auth.router');
 const app = express();
 
 app.use(cors());
 
 app.use('/api/topics', topicRouter);
 app.use('/api/running', runningRouter);
-app.use('/api/auth', authRouter);
+// app.use('/api/auth', authRouter);
 
 const port = process.env.PORT || 3001;
 
