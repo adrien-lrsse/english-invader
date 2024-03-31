9d051a3e1d9eaf17f13da311cd7173d9d3ef6d21
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-03-31 14:26:49 +0200

    [style] home page

--- client/src/components/LaunchGame/LaunchGame.jsx
+++ client/src/components/LaunchGame/LaunchGame.jsx
@@ -3,7 +3,9 @@ import './launchGame.css';
 const LaunchGame = () => {
     return ( 
         <div className="launchGame">
-            <a href="/game">Run Open Game</a>
+            <p> / Space Invader is a classic arcade game that has been reinvented in many different ways over the years. One such variation is a word-based game that challenges players to translate falling words before they reach the bottom of the screen.</p>
+            <p> / In this game, players take on the role of a linguistic defender, armed with a translation tool instead of a laser cannon. The objective is to correctly translate as many words as possible before they fall to the bottom of the screen.</p>
+            <a  href="/game">invade a classic topic</a>
         </div>
      );
 }
